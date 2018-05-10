28bcbdb1009ff725d8c8fd0bc4f8d287095b067a
yatingkun <652111934@qq.com>
2018-05-10 09:27:47 +0800

    做了一些优化

--- page1/page1.js
+++ page1/page1.js
@@ -2,17 +2,21 @@ var textdata = require("../textdata/textdata")
 var disabled = 'disabled';//控制提交按钮是否可按，为空字符串时可按
 var counter = 1;//记录已答题数
 var currentId = [];//记录每次点击的题目的id;
-var chooseId=[];//记录答题的选项
-var score=0;// 记录分数；
-var right=0;//记录正答数目；
-var error=0;//记录错答数目
+var chooseId = [];//记录答题的选项
+var score = 0;// 记录分数；
+var right = 0;//记录正答数目；
+var error = 0;//记录错答数目
 Page({
   data: {
     minute: 10,
     second: 0,
     disabled: "",
-    Interval:"", //转接计时器事件句柄
-    reslut:""
+    Interval: "", //转接计时器事件句柄
+    reslut: "",
+    state:true,//控制choose事件是否生效
+    hiddenmodalput: true,//控制modal的显隐状态，true为隐藏
+    name: "",//记录学生的名字
+    studentid: ""//记录学生的学号
   },
   onLoad: function () {
     //var that = this;
@@ -39,9 +43,9 @@ Page({
    */
   onReady: function () {
     var that = this;
-    var iy=setInterval(function () { that.timer() }, 1000);
+    var iy = setInterval(function () { that.timer() }, 1000);
     that.setData({
-      Interval:iy
+      Interval: iy
     });
   },
 
@@ -80,11 +84,12 @@ Page({
 
   },
   choose: function (event) {
-    var id = event.currentTarget.dataset.id;
-    var num = event.currentTarget.dataset.num;
-    /**判断是否已做完所有题目，做完提交按钮才可按**/
-    if (counter) {
-      chooseId[num]=id;//记录每一题的选项；
+    if (this.data.state) {
+      var id = event.currentTarget.dataset.id;
+      var num = event.currentTarget.dataset.num;
+      /**判断是否已做完所有题目，做完提交按钮才可按**/
+
+      chooseId[num] = id;//记录每一题的选项；
       currentId.push(num);
       if (currentId.length == 2) {
         if (currentId[0] == currentId[1]) {
@@ -100,37 +105,37 @@ Page({
           disabled: disabled
         });
       }
-      
-    }
-    /***进行各题之间的单项选择,给出相应显示*/
-    if (id == 1) {
-      textdata.textdata[num].imageurl.imageurlA = "../images/true.png";
-      textdata.textdata[num].imageurl.imageurlB = "";
-      textdata.textdata[num].imageurl.imageurlC = "";
-      textdata.textdata[num].imageurl.imageurlD = "";
-    }
-    else if (id == 2) {
-      textdata.textdata[num].imageurl.imageurlA = "";
-      textdata.textdata[num].imageurl.imageurlB = "../images/true.png";
-      textdata.textdata[num].imageurl.imageurlC = "";
-      textdata.textdata[num].imageurl.imageurlD = "";
-    }
-    else if (id == 3) {
-      textdata.textdata[num].imageurl.imageurlA = "";
-      textdata.textdata[num].imageurl.imageurlB = "";
-      textdata.textdata[num].imageurl.imageurlC = "../images/true.png";
-      textdata.textdata[num].imageurl.imageurlD = "";
-    }
-    else if (id == 4) {
-      textdata.textdata[num].imageurl.imageurlA = "";
-      textdata.textdata[num].imageurl.imageurlB = "";
-      textdata.textdata[num].imageurl.imageurlC = "";
-      textdata.textdata[num].imageurl.imageurlD = "../images/true.png";
-    }
-    this.setData({
-      textdata: textdata.textdata
-    })
 
+
+      /***进行各题之间的单项选择,给出相应显示*/
+      if (id == 1) {
+        textdata.textdata[num].imageurl.imageurlA = "../images/true.png";
+        textdata.textdata[num].imageurl.imageurlB = "";
+        textdata.textdata[num].imageurl.imageurlC = "";
+        textdata.textdata[num].imageurl.imageurlD = "";
+      }
+      else if (id == 2) {
+        textdata.textdata[num].imageurl.imageurlA = "";
+        textdata.textdata[num].imageurl.imageurlB = "../images/true.png";
+        textdata.textdata[num].imageurl.imageurlC = "";
+        textdata.textdata[num].imageurl.imageurlD = "";
+      }
+      else if (id == 3) {
+        textdata.textdata[num].imageurl.imageurlA = "";
+        textdata.textdata[num].imageurl.imageurlB = "";
+        textdata.textdata[num].imageurl.imageurlC = "../images/true.png";
+        textdata.textdata[num].imageurl.imageurlD = "";
+      }
+      else if (id == 4) {
+        textdata.textdata[num].imageurl.imageurlA = "";
+        textdata.textdata[num].imageurl.imageurlB = "";
+        textdata.textdata[num].imageurl.imageurlC = "";
+        textdata.textdata[num].imageurl.imageurlD = "../images/true.png";
+      }
+      this.setData({
+        textdata: textdata.textdata
+      })
+    }
   },
   /*倒计时函数*/
   timer: function () {
@@ -147,54 +152,85 @@ Page({
     }
   },
   /**提交函数，统计分数，给出提示 */
-submit:function(){
-  var that = this;
-  textdata.textdata.forEach((item ,index)=>{
-    if (item.answer == chooseId[index]) {
-      score += 3;
-      right++;
-    }else{
-      error++;
-      if (chooseId[index] == 1) { 
-        item.imageurl.imageurlA ="../images/false.png";
-        if (item.answer == 2) { item.imageurl.imageurlB = "../images/true.png" };
-        if (item.answer == 3) { item.imageurl.imageurlC = "../images/true.png"  };
-        if (item.answer == 4) { item.imageurl.imageurlD = "../images/true.png"  };
+  submit: function () {
+    var that = this;
+    textdata.textdata.forEach((item, index) => {
+      if (item.answer == chooseId[index]) {
+        score += 3;
+        right++;
+      } else {
+        error++;
+        if (chooseId[index] == 1) {
+          item.imageurl.imageurlA = "../images/false.png";
+          if (item.answer == 2) { item.imageurl.imageurlB = "../images/true.png" };
+          if (item.answer == 3) { item.imageurl.imageurlC = "../images/true.png" };
+          if (item.answer == 4) { item.imageurl.imageurlD = "../images/true.png" };
         };
-      if (chooseId[index] == 2) { 
-        item.imageurl.imageurlB = "../images/false.png";
-        if (item.answer == 1) { item.imageurl.imageurlB = "../images/true.png" };
-        if (item.answer == 3) { item.imageurl.imageurlC = "../images/true.png" };
-        if (item.answer == 4) { item.imageurl.imageurlD = "../images/true.png" };
+        if (chooseId[index] == 2) {
+          item.imageurl.imageurlB = "../images/false.png";
+          if (item.answer == 1) { item.imageurl.imageurlB = "../images/true.png" };
+          if (item.answer == 3) { item.imageurl.imageurlC = "../images/true.png" };
+          if (item.answer == 4) { item.imageurl.imageurlD = "../images/true.png" };
         };
-      if (chooseId[index] == 3) { 
-        item.imageurl.imageurlC = "../images/false.png";
-        if (item.answer == 1) { item.imageurl.imageurlB = "../images/true.png" };
-        if (item.answer == 2) { item.imageurl.imageurlC = "../images/true.png" };
-        if (item.answer == 4) { item.imageurl.imageurlD = "../images/true.png" };
+        if (chooseId[index] == 3) {
+          item.imageurl.imageurlC = "../images/false.png";
+          if (item.answer == 1) { item.imageurl.imageurlB = "../images/true.png" };
+          if (item.answer == 2) { item.imageurl.imageurlC = "../images/true.png" };
+          if (item.answer == 4) { item.imageurl.imageurlD = "../images/true.png" };
         };
-      if (chooseId[index] == 4) { 
-        item.imageurl.imageurlD = "../images/false.png";
-        if (item.answer == 1) { item.imageurl.imageurlB = "../images/true.png" };
-        if (item.answer == 2) { item.imageurl.imageurlC = "../images/true.png" };
-        if (item.answer == 3) { item.imageurl.imageurlD = "../images/true.png" };
+        if (chooseId[index] == 4) {
+          item.imageurl.imageurlD = "../images/false.png";
+          if (item.answer == 1) { item.imageurl.imageurlB = "../images/true.png" };
+          if (item.answer == 2) { item.imageurl.imageurlC = "../images/true.png" };
+          if (item.answer == 3) { item.imageurl.imageurlD = "../images/true.png" };
         };
-      
-    }
-    
-  }); 
-  disabled='disable';
-  wx.pageScrollTo({
-    scrollTop: 0
-  })
-  this.setData({
-    score:score,
-    disabled:disabled ,
-    textdata:textdata.textdata ,
-    right:right,
-    error:error,
-    reslut: "true"
-  });
-  clearInterval(that.data.Interval);//停止计时
-}
+
+      }
+
+    });
+    disabled = 'disable';
+    this.setData({
+      score: score,
+      disabled: disabled,
+      textdata: textdata.textdata,
+      right: right,
+      error: error,
+      reslut: "true",
+      state:false,
+      hiddenmodalput: !this.data.hiddenmodalput
+    });
+    clearInterval(that.data.Interval);//停止计时
+  },
+  userNameInput: function (e) {
+    this.setData({
+      name: e.detail.value
+    })
+  },
+  studentidInput: function (e) {
+    this.setData({
+      studentid: e.detail.value
+    })
+  },
+  //取消按钮  
+  cancel: function () {
+    this.setData({
+      name: "",
+      studentid: ""
+    })
+  },
+  //确认  
+  confirm: function () {
+    wx.showToast({
+      title: '提交成功',
+      icon: 'success',
+      duration: 1000
+    });
+    this.setData({
+      hiddenmodalput: true
+    });
+    wx.pageScrollTo({
+      scrollTop: 0
+    })
+    console.log("姓名：" + this.data.name + " 学号：" + this.data.studentid);
+  }    
 })
--- page2/page2.js
+++ page2/page2.js
@@ -1,5 +1,80 @@
-// page3.js
 
+
+
+//获取应用实例  
+var app = getApp()
 Page({
-  
+  /*showok: function () {
+    wx.showToast({
+      title: '成功',
+      icon: 'success',
+      duration: 2000
+    })
+  },
+  modalcnt: function () {
+    wx.showModal({
+      title: '提示',
+      content: '这是一个模态弹窗',
+      success: function (res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
+  } ,
+  actioncnt: function () {
+    wx.showActionSheet({
+      itemList: ['A', 'B', 'C'],
+      success: function (res) {
+        console.log(res.tapIndex)
+      },
+      fail: function (res) {
+        console.log(res.errMsg)
+      }
+    })
+  }*/
+  data: {
+    hiddenmodalput: true ,//控制modal的显隐状态，true为隐藏
+    name:"",//记录学生的名字
+    studentid:""//记录学生的学号
+    //可以通过hidden是否掩藏弹出框的属性，来指定那个弹出框  
+  },
+  //点击按钮痰喘指定的hiddenmodalput弹出框  
+  modalinput: function () {
+    this.setData({
+      hiddenmodalput: !this.data.hiddenmodalput
+      
+    })
+  },
+  userNameInput: function (e) {
+    this.setData({
+      name: e.detail.value
+    })
+  },
+ studentidInput: function (e) {
+    this.setData({
+      studentid: e.detail.value
+    })
+  },
+  //取消按钮  
+  cancel: function () {
+    this.setData({
+      name: "",
+      studentid: ""
+    })
+  },
+  //确认  
+  confirm: function () {
+    wx.showToast({
+      title: '提交成功',
+      icon: 'success',
+      duration: 1000
+    })
+    this.setData({
+      hiddenmodalput: true
+    })
+    console.log("姓名：" + this.data.name + " 学号：" + this.data.studentid);
+  }    
 })
